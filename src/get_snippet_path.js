074094428bd303ea9b3cfd9a5d8100dcc5397947
David Elliott <delliott@findmypast.com>
2017-03-06 15:49:53 +0000

    Added warning message when snippets folder no longer exists.

--- src/get_snippet_path.js
+++ src/get_snippet_path.js
@@ -7,16 +7,20 @@ const walk = require('fs-walk');
 function getSnippetPath(startingPath, snippetName) {
   let returnDir = undefined;
 
+  if (!fs.existsSync(startingPath)) {
+      console.log('Path cannot be found in ~/.turingsnip : ' +
+                  startingPath);
+      return undefined;
+  }
   if (!startingPath) {
     return undefined;
   }
   walk.walkSync(startingPath, function(basedir, filename, stat, next) {
-
     if (filename === snippetName)
     {
       returnDir = basedir;
     }
-    if (filename.startsWith(snippetName + "."))
+    if (filename.startsWith(snippetName + '.'))
     {
       returnDir = basedir;
     }
