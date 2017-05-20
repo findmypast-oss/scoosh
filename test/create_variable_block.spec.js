f7bb22374d586bd53a2cc5f87e17d012e9ba79f8
David Elliott <dave.elliott@codevelop.co.uk>
2017-05-20 19:15:40 +0100

    Looking better

--- test/create_variable_block.spec.js
+++ test/create_variable_block.spec.js
@@ -1,3 +1,5 @@
+const chai = require('chai');
+
 const {
   createVariableBlock,
   readSnippetConfiguration,
@@ -13,33 +15,33 @@ describe('createVariableBlock', () => {
       readSnippetConfiguration('testdata/snippets/ecmascript/newcomponent.json'),
       (answers, templateFile) => {
         expect(answers).to.exist();
-      },
+      }
     );
   });
-  describe('filterParametersPassedFromCommandLine removes already set variables from the inquirer questions', () => {
-    it('removes a given command line parameter ', () => {
-      const questions = [
-        {
-          name: 'ComponentName',
-          type: 'input',
-          default: '<SPECIFYNAMEHERE>',
-          message: 'the component name of the blahdeblah',
-        },
-      ];
-      const actual = filterParametersPassedFromCommandLine({ ComponentName: 'test' }, questions);
-      expect(actual).to.be.empty;
-    });
-    it('does not remove when no command line parameters match', () => {
-      const questions = [
-        {
-          name: 'ComponentName',
-          type: 'input',
-          default: '<SPECIFYNAMEHERE>',
-          message: 'the component name of the blahdeblah',
-        },
-      ];
-      const actual = filterParametersPassedFromCommandLine({ Name: 'test' }, questions);
-      expect(actual).to.deep.equal(questions);
-    });
+});
+describe('filterParametersPassedFromCommandLine removes already set variables from the inquirer questions', () => {
+  it('removes a given command line parameter ', () => {
+    const questions = [
+      {
+        name: 'ComponentName',
+        type: 'input',
+        default: '<SPECIFYNAMEHERE>',
+        message: 'the component name of the blahdeblah',
+      },
+    ];
+    const actual = filterParametersPassedFromCommandLine({ ComponentName: 'test' }, questions);
+    expect(actual).to.be.empty;
+  });
+  it('does not remove when no command line parameters match', () => {
+    const questions = [
+      {
+        name: 'ComponentName',
+        type: 'input',
+        default: '<SPECIFYNAMEHERE>',
+        message: 'the component name of the blahdeblah',
+      },
+    ];
+    const actual = filterParametersPassedFromCommandLine({ Name: 'test' }, questions);
+    expect(actual).to.deep.equal(questions);
   });
 });
