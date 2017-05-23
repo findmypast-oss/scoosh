487901bfec5c1104f402c4fea374ace26f79502d
David Elliott <dave.elliott@codevelop.co.uk>
2017-05-23 22:56:02 +0100

    Stopped console.log errors in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
-    env: {
-        es6: true,
-        node: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-        sourceType: 'module',
-    },
-    rules: {
-        indent: ['error', 2],
-        'linebreak-style': ['error', 'unix'],
-        quotes: ['error', 'single'],
-        semi: ['error', 'always'],
-    },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };
